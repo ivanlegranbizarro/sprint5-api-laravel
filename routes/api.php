e3634a6118b7395013f039957b221b545207ae30
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-10 19:40:17 +0200

    setup de policies

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('users')->group(function () {
+  Route::post('login', [UserController::class, 'login'])->name('login');
+  Route::post('store', [UserController::class, 'store'])->name('store');
+  Route::apiResource('/', UserController::class)->middleware('auth:api')->except('store');
+});
