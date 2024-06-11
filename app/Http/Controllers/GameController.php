9ffaa5a9180854eb5662c2be9354e86ef53c3778
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-11 16:44:06 +0200

    setup --all --api de la entidad Game

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreGameRequest;
+use App\Http\Requests\UpdateGameRequest;
+use App\Models\Game;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGameRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Game $game)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGameRequest $request, Game $game)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Game $game)
+    {
+        //
+    }
+}
