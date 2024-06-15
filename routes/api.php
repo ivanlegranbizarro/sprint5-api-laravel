9e687943273e3ddf1da046bd15fcb4f3e58245f7
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-15 09:21:17 +0200

    mejorar agrupación de las rutas

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,23 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('players')->group(function () {
   Route::post('login', [UserController::class, 'login'])->name('login');
   Route::post('', [UserController::class, 'store'])->name('store');
-  Route::apiResource('/', UserController::class)->middleware('auth:api')->except('store', 'destroy');
-  Route::post('games', [GameController::class, 'playGame'])->name('playGame')->middleware('auth:api');
-  Route::get('games', [GameController::class, 'playerIndex'])->name('playerIndex')->middleware('auth:api');
-  Route::get('games/admin', [GameController::class, 'adminIndex'])->name('adminIndex')->middleware('auth:api');
-  Route::get('{user}/games', [GameController::class, 'show'])->name('showPlayerGames')->middleware('auth:api');
-  Route::delete('{user}/games', [GameController::class, 'destroy'])->name('destroyPlayerGames')->middleware('auth:api');
-  Route::get('ranking', [UserController::class, 'ranking'])->name('rankingAllPlayers')->middleware('auth:api');
-  Route::get('ranking/winner', [UserController::class, 'bestPlayer'])->name('rankingBestPlayer')->middleware('auth:api');
-  Route::get('ranking/loser', [UserController::class, 'worstPlayer'])->name('rankingWorstPlayer')->middleware('auth:api');
+  Route::apiResource('/', UserController::class)
+    ->middleware('auth:api')
+    ->except('store', 'destroy');
+
+  Route::middleware('auth:api')->group(function () {
+    Route::prefix('games')->group(function () {
+      Route::post('/', [GameController::class, 'playGame'])->name('playGame');
+      Route::get('/', [GameController::class, 'playerIndex'])->name('playerIndex');
+      Route::get('admin', [GameController::class, 'adminIndex'])->name('adminIndex');
+      Route::get('{user}', [GameController::class, 'show'])->name('showPlayerGames');
+      Route::delete('{user}', [GameController::class, 'destroy'])->name('destroyPlayerGames');
+    });
+
+    Route::prefix('ranking')->group(function () {
+      Route::get('/', [UserController::class, 'ranking'])->name('rankingAllPlayers');
+      Route::get('winner', [UserController::class, 'bestPlayer'])->name('rankingBestPlayer');
+      Route::get('loser', [UserController::class, 'worstPlayer'])->name('rankingWorstPlayer');
+    });
+  });
 });
